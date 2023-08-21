28b4a62d2db3958a23d28f62d1926cf3c12c8dcf
Yasser Mohamed <76072694+yasser74220@users.noreply.github.com>
2023-08-21 00:32:14 -0700

    initialize ApiService generic function

--- Currency Conversion/Controllers/ViewController.swift
+++ Currency Conversion/Controllers/ViewController.swift
@@ -5,15 +5,13 @@
 //  Created by Yasser Mohamed on 20/08/2023.
 //
 
+import Alamofire
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ Currency Conversion/Models/model.swift
@@ -0,0 +1,11 @@
+//
+//  model.swift
+//  Currency Conversion
+//
+//  Created by Yasser Mohamed on 21/08/2023.
+//
+
+import Foundation
+class model:Codable {
+    
+}
